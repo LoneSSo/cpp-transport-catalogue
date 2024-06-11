2c6f10d6a500b4bd184dacddae9d090aba47a75b
LoneSSo <baltoshka@gmail.com>
2024-06-11 20:03:24 +0300

    feat: add json support, add svg support

--- transport-catalogue/map_renderer.cpp
+++ transport-catalogue/map_renderer.cpp
@@ -1,11 +1,7 @@
 #include "map_renderer.h"
 
 using namespace renderer;
-/*
- * В этом файле вы можете разместить код, отвечающий за визуализацию карты маршрутов в формате SVG.
- * Визуализация маршртутов вам понадобится во второй части итогового проекта.
- * Пока можете оставить файл пустым.
- */
+
 bool renderer::IsZero(double value) {
     return std::abs(value) < EPSILON;
 }
